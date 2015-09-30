eba2621fbb0f76c5f7f7b03b39ff1ac7a2abb277
Jay Schultz <jayjschultz@gmail.com>
2015-09-29 23:28:41 -0500

    changes

--- ToDo/ToDo/ngApp/controllers.js
+++
@@ -1,1 +0,0 @@
-//# sourceMappingURL=controllers.js.map
--- ToDo/ToDo/ngApp/myApp.js
+++
@@ -1,6 +0,0 @@
-var MyApp;
-(function (MyApp) {
-    angular.module('MyApp', ['$http'])
-        .constant('WebAPI', '/api/todos');
-})(MyApp || (MyApp = {}));
-//# sourceMappingURL=myApp.js.map
